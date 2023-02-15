11e3f91656a9c61c3ec4792e510ce63f2976e35c
Christina <stinacon@gmail.com>
2023-02-15 13:14:03 -0500

    added notes about where to write the code and to erase 'pass'

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+# use a dictionary for my word: count
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -8,6 +10,9 @@ STOP_WORDS = [
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
     pass
+# i'm going to remove this word - it is a placeholder to make an empty function not break
+# so i remove pass then put my code inside the above function!
+# the existing code will call the function in later lines
 
 
 if __name__ == "__main__":
